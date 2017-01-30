9110eead7d490b7337ec36a0c4cb6af2cd165c4a
RaJiska <dodo.lasticot@gmail.com>
2017-01-30 10:07:24 +0100

    MapPos{isvalid, toint}

---
+++ src/map/map_pos_isvalid.c
@@ -0,0 +1,22 @@
+/*
+** map_pos_isvalid.c for navy in /home/rajiska/Epitech/PSU_2016_navy/src/map
+** 
+** Made by Corlouer Doriann
+** Login   <corlouer_d@epitech.net>
+** 
+** Started on  Mon Jan 30 10:00:47 2017 Corlouer Doriann
+** Last update Mon Jan 30 10:06:29 2017 Corlouer Doriann
+*/
+
+#include "../../include/navy.h"
+
+int	map_pos_isvalid(const char *pos)
+{
+  if (my_strlen(pos) != 2)
+    return FALSE;
+  if (pos[0] < 'A' || pos[0] > (('A') + NAVY_MAP_W - 1))
+    return FALSE;
+  if (pos[1] < '1' || pos[1] > '8')
+    return FALSE;
+  return TRUE;
+}
